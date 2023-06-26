6433615ca3f732e556c5eb86df49965543b5f6f6
Marceline-sama <oninoniisan@gmail.com>
2023-06-26 20:23:29 -0300

    Add files via upload
    atividade pronta

---
+++ lab02 Claybson Jordão/lab02/lista_array.py
@@ -0,0 +1,39 @@
+from estrutura_elementar import estrutura_elementar
+
+
+class ArrayList(estrutura_elementar):
+    def __init__(self):
+        pass
+
+    def esta_vazio(self) -> bool:#fazer essa
+        lista = []              
+        for i in range(len(lista)):
+            if not lista[i]:
+                return True 
+            else:return False       
+        
+
+    def inserir(self, item):#fazer essa
+        lista = [1, 2, 3, 4]
+        
+
+    def remove(self, item):
+        pass
+
+    def tamanho(self) -> int:#fazer essa
+        lista = []
+        for i in range (len(lista)):
+            print(i)
+        return 1
+
+    def limpa(self):
+        pass
+
+    def procura(self, item) -> bool:
+        return False
+
+    def indice_de(self, item):
+        return 1
+
+    def recupera_indice(self, index):
+        return 1
